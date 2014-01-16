4acdecb75ee99e8b77c4c739eed3d812ffc92057
Tobias Kahan <tobyk100@gmail.com>
2014-01-15 17:33:56 -0800

    Sudoize npm commands.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo npm install -g grunt-cli
-npm install
-script/full-build
+sudo npm install
+sudo script/full-build
 mkdir -p ./dist
 (cd ./build && tar -zcvf ../dist/blockly-mooc.tgz ./package)
