1d1e48798eec88b5f93fd2b6e2cf7519d6d061a9
Tobias Kahan <tobyk100@gmail.com>
2014-02-06 19:09:49 -0800

    Modify drawAnswer so it can go from levels.js configured answer or from server provided answer.

--- src/turtle/turtle.js
+++ src/turtle/turtle.js
@@ -136,7 +136,7 @@ Turtle.init = function(config) {
     // Set their initial contents.
     Turtle.loadTurtle();
     Turtle.drawImages();
-    Turtle.drawAnswer();
+    Turtle.drawAnswer(level.solutionBlocks);
 
     // Adjust visualization and belowVisualization width.
     var drawAreaWidth = config.getDisplayWidth();
@@ -155,8 +155,19 @@ Turtle.init = function(config) {
 /**
  * On startup draw the expected answer and save it to the answer canvas.
  */
-Turtle.drawAnswer = function() {
-  BlocklyApps.log = level.answer;
+Turtle.drawAnswer = function(blocks) {
+  if (blocks) {
+    var domBlocks = Blockly.Xml.textToDom(blocks);
+    Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, domBlocks);
+    Turtle.code = Blockly.Generator.workspaceToCode('JavaScript');
+    codegen.evalWith(Turtle.code, {
+      BlocklyApps: BlocklyApps,
+      Turtle: api
+    });
+    Blockly.mainWorkspace.clear();
+  } else {
+    BlocklyApps.log = level.answer;
+  }
   BlocklyApps.reset();
   while (BlocklyApps.log.length) {
     var tuple = BlocklyApps.log.shift();
