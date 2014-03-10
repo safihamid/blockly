59931ef1658ca38572876d65340d2a895d608de3
Brent Van Minnen <bjvanminnen@gmail.com>
2014-03-10 14:19:10 -0700

    cleanup test code

--- test/test.js
+++ test/test.js
@@ -15,41 +15,12 @@ var wrench = require('wrench');
 
 var child_process = require('child_process');
 
-// Loads a test collection at path an runs all the tests specified in it.
-var runTestCollection = function (path) {
-  var testCollection = require('./' + path);
-  var app = testCollection.app;
-
-  var levels = require('../build/js/' + app + '/' + testCollection.levelFile);
-  var level = levels[testCollection.levelId];
-
-  var exceptions, messages;
-  describe(path, function () {
-    testCollection.tests.forEach(function (testData, index) {
-      it(testData.description, function (done) {
-        // can specify a test specific timeout in json file.
-        if (testData.timeout !== undefined) {
-          this.timeout(testData.timeout);
-        }
-        if (process.execArgv.indexOf('--debug') !== -1 ||
-          process.execArgv.indexOf('--debug-brk') !== -1) {
-          // Don't timeout while we're debugging
-          this.timeout(0);
-        }
-
-        child_process.exec('node test/executor ' + path + ' ' +  index,
-          {}, function (error, stdout, stderr) {
-            assert.equal(error, null);
-            assert(stderr === "", '\n' + stderr);
-            done();
-        });
-      });
-    });
-  });
-};
+getTestCollections('./test').forEach(function (path) {
+  runTestCollection(path);
+});
 
 // Get all json files under directory path
-var getTestCollections = function (directory) {
+function getTestCollections (directory) {
   var files = wrench.readdirSyncRecursive(directory);
   var testCollections = [];
   files.forEach(function (file) {
@@ -58,8 +29,41 @@ var getTestCollections = function (directory) {
     }
   });
   return testCollections;
-};
+}
 
-getTestCollections('./test').forEach(function (path) {
-  runTestCollection(path);
-});
+// Loads a test collection at path an runs all the tests specified in it.
+function runTestCollection (path) {
+  var testCollection = require('./' + path);
+  var app = testCollection.app;
+
+  var levels = require('../build/js/' + app + '/' + testCollection.levelFile);
+  var level = levels[testCollection.levelId];
+
+  describe(path, function () {
+    testCollection.tests.forEach(function (testData, index) {
+      runTest(path, testData, index);
+    });
+  });
+}
+
+function runTest (path, testData, index) {
+  it(testData.description, function (done) {
+    // can specify a test specific timeout in json file.
+    if (testData.timeout !== undefined) {
+      this.timeout(testData.timeout);
+    }
+    if (process.execArgv.indexOf('--debug') !== -1 ||
+      process.execArgv.indexOf('--debug-brk') !== -1) {
+      // Don't timeout while we're debugging
+      this.timeout(0);
+    }
+
+    // run executor in it's own node process
+    child_process.exec('node test/executor ' + path + ' ' +  index,
+      {}, function (error, stdout, stderr) {
+        assert.equal(error, null);
+        assert(stderr === "", '\n' + stderr);
+        done();
+    });
+  });
+}
