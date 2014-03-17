a36737aa6a239a0f58e6475cc44f1da4d5fea55c
deploy-code-org <dev@code.org>
2014-03-17 21:22:08 +0000

    Tweaks.

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@ sudo npm install -g grunt-cli
 sudo npm install
 sudo script/full-build
 mkdir -p ./dist
-(cd ./build && tar -zcvf ../dist/blockly-mooc.tgz ./package)
-git rev-parse HEAD > .cache_bust
+(cd ./build && tar -zcvf ../blockly-mooc.tgz ./package)
