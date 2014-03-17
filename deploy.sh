433f86f7e806a487316bc019230bab0be6a17274
Geoffrey Elliott <geoffrey.elliott@me.com>
2014-03-17 14:19:16 -0700

    Check in blockly to dashboard after build.

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ sudo script/full-build
 mkdir -p ./dist
 (cd ./build && tar -zcvf ../dist/blockly-mooc.tgz ./package)
 git rev-parse HEAD > .cache_bust
+
+(cd ../dashboard/public; git rm -rf blockly-dist; tar -zxf ../../blockly/blockly-mooc.tgz; mv package blockly-dist; git add blockly-dist; git commit -m "Automatically upgraded blockly-dist."; git push)
