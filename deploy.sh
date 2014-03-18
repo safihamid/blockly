a948c36b981f5f602a6b43b69dba6128a054c4b7
Geoffrey Elliott <geoffrey.elliott@me.com>
2014-03-17 22:01:05 -0700

    Pull before integrating blockly.

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,4 @@ sudo npm install
 sudo script/full-build
 (cd ./build && tar -zcvf ../blockly-mooc.tgz ./package)
 
-(cd ../dashboard/public; git rm -rf blockly-dist; tar -zxf ../../blockly/blockly-mooc.tgz; mv package blockly-dist; git add blockly-dist; git commit -m "Automatically upgraded blockly-dist."; git push)
-
+(cd ../dashboard/public; git pull; git rm -rf blockly-dist; tar -zxf ../../blockly/blockly-mooc.tgz; mv package blockly-dist; git add blockly-dist; git commit -m "Automatically upgraded blockly-dist."; git push)
