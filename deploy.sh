8b8774a20cf235d1745bb27d671f1d18d836bf64
Brent Van Minnen <bjvanminnen@gmail.com>
2014-03-19 13:34:12 -0700

    switch deploy script to ruby and have it run grunt with no args

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env ruby
+require_relative '../util'
 
-sudo npm install -g grunt-cli
-sudo npm install
-sudo script/full-build
-(cd ./build && tar -zcvf ../blockly-mooc.tgz ./package)
+system_ex 'sudo npm install -g grunt-cli'
+system_ex 'sudo npm install'
+system_ex 'sudo MOOC_LOCALIZE=1 grunt'
+system_ex '(cd ./build && tar -zcvf ../blockly-mooc.tgz ./package)'
 
-(cd ../dashboard/public; git pull; git rm -rf blockly-dist; tar -zxf ../../blockly/blockly-mooc.tgz; mv package blockly-dist; git add blockly-dist; git commit -m "Automatically upgraded blockly-dist."; git push)
+system_ex '(cd ../dashboard/public; git pull; git rm -rf blockly-dist; tar -zxf ../../blockly/blockly-mooc.tgz; mv package blockly-dist; git add blockly-dist; git commit -m "Automatically upgraded blockly-dist."; git push)'
