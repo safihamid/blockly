5689615cb670506c57b15e6f81240fafadf16347
Brian Jordan <bcjordan@gmail.com>
2014-04-21 14:32:54 -0700

    Review revisions: use constants for image parameters. Point out generator re-use.

--- src/blocksCommon.js
+++ src/blocksCommon.js
@@ -3,12 +3,18 @@
  */
 'use strict';
 
+var REPEAT_IMAGE_URL = 'media/sharedBlocks/repeat.png';
+var REPEAT_IMAGE_WIDTH = 53;
+var REPEAT_IMAGE_HEIGHT = 57;
+
 /**
  * Install extensions to Blockly's language and JavaScript generator
  * @param blockly instance of Blockly
  */
 exports.install = function(blockly) {
+  // Re-uses the repeat block generator from core
   blockly.JavaScript.controls_repeat_simplified = blockly.JavaScript.controls_repeat;
+
   blockly.Blocks.controls_repeat_simplified = {
     // Repeat n times (internal number) with simplified UI
     init: function() {
@@ -16,7 +22,7 @@ exports.install = function(blockly) {
       this.setHSV(322, 0.90, 0.95);
       this.appendStatementInput('DO')
         .appendTitle(new blockly.FieldImage(
-          blockly.assetUrl('media/sharedBlocks/repeat.png'), 53, 50));
+          blockly.assetUrl(REPEAT_IMAGE_URL), REPEAT_IMAGE_WIDTH, REPEAT_IMAGE_HEIGHT));
       this.appendDummyInput()
         .appendTitle(blockly.Msg.CONTROLS_REPEAT_TITLE_REPEAT)
         .appendTitle(new Blockly.FieldTextInput('10',
